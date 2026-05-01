6796c42dcabb6795a86db1625d8990a349734853
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2026-05-01 15:50:22 -0300

    Implementação da busca binária

---
+++ algoritmos/busca_binaria.cpp
@@ -0,0 +1,28 @@
+#include "busca_binaria.h"
+#include <algorithm>
+
+int buscaBinaria(int v[], int inicio, int fim, int valor) {
+
+    while (inicio <= fim) {
+
+        int meio = (inicio + fim) / 2;
+
+        if (v[meio] == valor)
+            return meio;
+
+        else if (v[meio] < valor)
+            inicio = meio + 1;
+
+        else
+            fim = meio - 1;
+    }
+
+    return -1;
+}
+
+void buscaBinariaAdaptada(int v[], int n) {
+
+    int valor = v[n / 2];
+
+    buscaBinaria(v, 0, n - 1, valor);
+}
---
+++ algoritmos/busca_binaria.h
@@ -0,0 +1,6 @@
+#ifndef BUSCA_BINARIA_H
+#define BUSCA_BINARIA_H
+
+void buscaBinariaAdaptada(int v[], int n);
+
+#endif
